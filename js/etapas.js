ce74284870c5f0cb9c3752f9345d7759d17194b1
Luan-Novais <luan.novaiss@outlook.com>
2021-01-19 08:41:52 -0300

    manipulando objetos do html com JS

---
+++ js/etapas.js
@@ -0,0 +1,49 @@
+let etapas = [
+    {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+        {
+            numero: 38111,
+            nome: 'Fulado de tal',
+            partido: 'ABC',
+            fotos:[
+                {url:'38111.jpg', legenda:'vereador', small:false}
+            ]
+        },
+        {
+            numero: 77222,
+            nome: 'Beltrano de Souza',
+            partido: 'DFEG',
+            fotos:[
+                {url:'31777.jpg', legenda:'vereador', small:false}
+            ]
+        },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:  {
+            numero: 99,
+            nome: 'Ciclano',
+            partido: 'ABC',
+            vice: 'Csis',
+            fotos:[
+                {url:'99.jpg', legenda:'Prefeito'},
+                {url:'99_2.jpg', legenda:'Vice-prefeito', small:true}
+            ]
+        }
+    },
+    {
+        numero: 84,
+        nome: 'Zuquinha',
+        partido: 'QERTT',
+        vice: 'Zezo',
+        fotos:[
+            {url:'84.jpg', legenda:'Prefeito'},
+            {url:'84_2.jpg', legenda:'Vice-prefeito', small:true}
+        ]
+    }
+    
+]
